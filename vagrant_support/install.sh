6cd9d6fdfb64325af466b7877d1e27f60a735f18
Chaitanya Mukka <chaitanya.mvs2007@gmail.com>
2017-06-10 23:56:55 +0530

    Minor changes and Updated README

--- vagrant_support/install.sh
+++ vagrant_support/install.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+
 echo "PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \$ '" >> /home/vagrant/.bashrc
+
 echo "cd /home/vagrant/swd" >>  /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
+
 cd /home/vagrant/swd
