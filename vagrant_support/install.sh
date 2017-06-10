c69edaa4d77eb5d46d0e9800879f46ab8e169bfc
Sebastin Santy <sebastinssanty@gmail.com>
2017-06-10 14:58:57 +0530

    Installation setup on a centos/7 vagrant machine

---
+++ vagrant_support/install.sh
@@ -0,0 +1,4 @@
+echo "PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \$ '" >> /home/vagrant/.bashrc
+echo "cd /home/vagrant/swd" >>  /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
+cd /home/vagrant/swd
