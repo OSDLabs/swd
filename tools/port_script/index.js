108614bf02c7f5b7ca03c450f458332ffbdc3552
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-09-26 17:18:59 +0530

    Complete the porting script framework
    - Generalized to make porting easier by changing only the transform.js
    file which is nothing but json.

--- tools/port_script/index.js
+++ tools/port_script/index.js
@@ -1,6 +1,9 @@
 const mysql = require('mysql');
 const config = require('./config.js');
 const transform = require('./transform.js');
+const _ = require('lodash');
+
+const DEBUG = 1;
 
 // Connect to the source database
 const source = mysql .createConnection({
@@ -27,43 +30,117 @@ dest.connect(function(err) {
 });
 
 
-for (var item in transform) {
-  console.log(transform[item].table.old);
-  source.query('SELECT * FROM ' + transform[item].table.old + ';',
-    function(err, res, fields) {
-      if (err) { throw err; }
-      console.log('\nCopying table ' + transform[item].table.old);
-      for (var i in res) {
-        let query = 'INSERT INTO ';
-        query += transform[item].table.new;
-        query += ' (';
+_.forEach(transform, (rule) => {
+  let select_query = null;
+  if (rule.table.old.union) {
+    console.log('union');
+    select_query = 'SELECT * FROM ' + rule.table.old.union[0] + ' INNER JOIN (';
 
-        // Load fields in order
-        let values = '';
-        let count = 0;
-        for (var newField in transform[item].fields) {
-          if (count != 0) {
-            query += ', ';
-            values += ', ';
-          }
-          query += newField;
+    let count = 0;
+    _.forEach(rule.table.old.union, (table) => {
 
-          // Add various data cleaning techniques here
-          values += '"' + res[i][transform[item].fields[newField]] + '"';
-          count++;
+      if (count == 0) {
+        console.log('hi');
+      } else if (count == 1) {
+        select_query += table;
+      } else {
+        select_query += ', ' + table;
+      }
+      count++;
+    });
+
+    select_query += ') ON (' + rule.table.old.on + ')';
+  } else {
+    select_query = 'SELECT * FROM ' + rule.table.old;
+  }
+  DEBUG && (select_query += ' LIMIT 5');
+  select_query += ';';
+  console.log(select_query);
+
+  // Run the query
+  source.query(select_query, function(err, tuples, fields) {
+
+    if (err) { throw err; }
+    console.log('\nCopying table ' + rule.table.new);
+
+    _.forEach(tuples, (tuple) => {
+
+      let query = 'INSERT INTO ';
+      query += rule.table.new;
+      query += ' (';
+
+      // Load fields in order
+      let values = '';
+      let count = 0;
+      _.forEach(rule.fields, (field, field_name) => {
+
+        // Add comma if not the first item
+        if (count != 0) {
+          query += ', ';
+          values += ', ';
         }
 
-        query += ') VALUES (' + values;
-        query += ');';
+        // Add field name to query
+        query += field_name;
 
-        // Run the query
-        dest.query(query, function(err, ans) {
-          if (err) { throw err; }
-          else {
-            console.log(query);
+        // TODO: Add various data cleaning techniques here
+        let attribute = field;
+
+        // check if there's a condition
+        if (attribute.if) {
+
+          if (condition_eval(attribute.if.condition, tuple)) {
+            values += '"' + attribute.if.pass.value + '"';
+          } else {
+            values += '"' + attribute.if.fail.value + '"';
           }
-        });
-      }
+
+        } else {
+          values += '"' + tuple[field] + '"';
+          count++;
+        }
+      }); // end forEach(fields)
+
+      query += ') VALUES (' + values;
+      query += ');';
+
+      // Run the query
+      DEBUG && console.log(query + '\n');
+      DEBUG || dest.query(query, function(err, ans) {
+        if (err) { throw err; }
+        else {
+          console.log(query);
+        }
+      });
+
+    }); // end of forEach(res)
+  });
+});
+
+function condition_eval(condition, data) {
+  if (condition.eval) {
+    let first = data[condition.eval.first_attribute] || condition.eval.first_value || null;
+    let second = data[condition.eval.second_attribute] || condition.eval.second_value || null;
+
+    switch (condition.eval.operator) {
+      case '==':
+      case '===':
+        if (first == second) {
+          return true;
+        } else {
+          return false;
+        }
+        break;
+      default:
+        throw 'Operator not supported';
     }
-  );
+  } else if (condition.or) {
+    // TODO: make this variable
+    return condition_eval(condition.or[0], data) || condition_eval(condition.or[1], data);
+  } else if (condition.and) {
+    // TODO: make this variable
+    return condition_eval(condition.or[0], data) && condition_eval(condition.or[1], data);
+  } else {
+    throw 'Could not find eval or a condition';
+  }
 }
